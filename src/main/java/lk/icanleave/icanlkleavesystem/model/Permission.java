d8add784ca1954e20d5ea52f65555026f6d03e8f
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-12-01 17:48:31 +0530

    mysql db dialect -> maria db dialect

---
+++ src/main/java/lk/icanleave/icanlkleavesystem/model/Permission.java
@@ -0,0 +1,24 @@
+package lk.icanleave.icanlkleavesystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int permission_id;
+
+    private String permission_name;
+}
--- src/main/java/lk/icanleave/icanlkleavesystem/model/Role.java
+++ src/main/java/lk/icanleave/icanlkleavesystem/model/Role.java
@@ -14,7 +14,6 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "Roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
